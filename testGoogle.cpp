33cd992b2ebbd68b701f76cf06b28b5c6ac47bb0
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-03-30 19:46:00 +0100

    added tests for word_counter methods(still need to add iterators and  << >> operators)

--- testGoogle.cpp
+++ testGoogle.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "entry.h"
+#include "word_counter.h"
 
 TEST(entry, default_contructor)
 {
@@ -163,4 +164,299 @@ TEST(findLastComma, test_no_comma)
 {
     std::string s = "[bba1]";
     ASSERT_EQ(findLastComma(s), -1);
+}
+
+TEST(word_counter, init_and_getters_1)
+{
+    // testing if does not throw an error
+    word_counter w = word_counter();
+}
+
+TEST(word_counter, init_and_getter_2)
+{
+    std::string s1 = "ccc";
+    entry e1(s1, 1);
+    std::string s2 = "bbb";
+    entry e2(s2, 2);
+    std::string s3 = "aaa";
+    entry e3(s3, 3);
+
+    std::vector<entry> arr = {e1, e2, e3};
+
+    word_counter wc(arr);
+    ASSERT_EQ(wc.isEmpty(), false);
+    ASSERT_EQ(wc.getList()[0] == e3, true);
+    ASSERT_EQ(wc.getList()[1] == e2, true);
+    ASSERT_EQ(wc.getList()[2] == e1, true);
+}
+
+TEST(word_counter, getIdx)
+{
+    std::string s1 = "ccc";
+    entry e1(s1, 1);
+    std::string s2 = "bbb";
+    entry e2(s2, 2);
+    std::string s3 = "aaa";
+    entry e3(s3, 3);
+
+    std::vector<entry> arr = {e1, e2, e3};
+
+    word_counter wc(arr);
+    ASSERT_EQ(wc.getIdx(s3), 0);
+    ASSERT_EQ(wc.getIdx(s2), 1);
+    ASSERT_EQ(wc.getIdx(s1), 2);
+
+    std::string s4 = "ggg";
+    ASSERT_EQ(wc.getIdx(s4), -1);
+}
+
+TEST(word_counter, getEntryReference)
+{
+    std::string s1 = "ccc";
+    entry e1(s1, 1);
+    std::string s2 = "bbb";
+    entry e2(s2, 2);
+    std::string s3 = "aaa";
+    entry e3(s3, 3);
+
+    std::vector<entry> arr = {e1, e2, e3};
+
+    word_counter wc(arr);
+    ASSERT_EQ(wc.getEntryReference(s3) == e3, true);
+    ASSERT_EQ(wc.getEntryReference(s2) == e2, true);
+    ASSERT_EQ(wc.getEntryReference(s1) == e1, true);
+
+    std::string s4 = "ggg";
+    ASSERT_THROW(wc.getEntryReference(s4), std::invalid_argument);
+}
+
+TEST(word_counter, isEmpty)
+{
+    std::string s1 = "ccc";
+    entry e1(s1, 1);
+    std::string s2 = "bbb";
+    entry e2(s2, 2);
+    std::string s3 = "aaa";
+    entry e3(s3, 3);
+
+    std::vector<entry> arr = {e1, e2, e3};
+
+    word_counter wc;
+    ASSERT_TRUE(wc.isEmpty());
+
+    wc.addWords(arr);
+    ASSERT_TRUE(!wc.isEmpty());
+}
+
+TEST(word_counter, hasWord)
+{
+    std::string s1 = "ccc";
+    entry e1(s1, 1);
+    std::string s2 = "bbb";
+    entry e2(s2, 2);
+    std::string s3 = "aaa";
+    entry e3(s3, 3);
+
+    std::vector<entry> arr = {e1, e2, e3};
+    word_counter wc(arr);
+
+    ASSERT_TRUE(wc.hasWord(s1));
+    ASSERT_TRUE(wc.hasWord(s2));
+    ASSERT_TRUE(wc.hasWord(s3));
+
+    std::string s4 = "ddd";
+    ASSERT_TRUE(!wc.hasWord(s4));
+}
+
+TEST(word_counter, highestLowestCount)
+{
+    std::string s1 = "ccc";
+    entry e1(s1, 1);
+    std::string s2 = "bbb";
+    entry e2(s2, 20);
+    std::string s3 = "aaa";
+    entry e3(s3, 3);
+
+    std::vector<entry> arr = {e1, e2, e3};
+    word_counter wc(arr);
+
+    ASSERT_TRUE(wc.highesCount() == e2);
+    ASSERT_TRUE(wc.lowestCount() == e2);
+}
+
+TEST(word_counter, numberOfWords)
+{
+    std::string s1 = "ccc";
+    entry e1(s1, 1);
+    std::string s2 = "bbb";
+    entry e2(s2, 20);
+    std::string s3 = "aaa";
+    entry e3(s3, 3);
+
+    std::vector<entry> arr = {e1, e2, e3};
+    word_counter wc(arr);
+
+    ASSERT_EQ(wc.numberOfWords(), 24);
+}
+
+TEST(word_counter, getList)
+{
+    std::string s1 = "ccc";
+    entry e1(s1, 1);
+    std::string s2 = "bbb";
+    entry e2(s2, 20);
+    std::string s3 = "aaa";
+    entry e3(s3, 3);
+
+    std::vector<entry> arr = {e1, e2, e3};
+    word_counter wc(arr);
+
+    ASSERT_TRUE(wc.getList()[0] == e3);
+    ASSERT_TRUE(wc.getList()[1] == e2);
+    ASSERT_TRUE(wc.getList()[2] == e1);
+}
+TEST(word_counter, addWord)
+{
+    std::string s1 = "ccc";
+    entry e1(s1, 1);
+    std::string s2 = "bbb";
+    entry e2(s2, 20);
+    std::string s3 = "aaa";
+    entry e3(s3, 3);
+
+    std::vector<entry> arr = {e1, e2, e3};
+    word_counter wc(arr);
+
+    std::string s4 = "aba";
+    wc.addWord(s4);
+
+    ASSERT_TRUE(wc.getList()[0] == e3);
+    ASSERT_TRUE(*wc.getList()[1] == s4);
+    ASSERT_TRUE(wc.getList()[2] == e2);
+    ASSERT_TRUE(wc.getList()[3] == e1);
+}
+
+TEST(word_counter, addWords_from_vector_of_strings)
+{
+    std::string s1 = "ccc";
+    entry e1(s1, 1);
+    std::string s2 = "bbb";
+    entry e2(s2, 20);
+    std::string s3 = "aaa";
+    entry e3(s3, 3);
+
+    std::vector<entry> arr = {e1, e2, e3};
+    word_counter wc(arr);
+
+    std::string s4 = "aba";
+    std::string s5 = "eee";
+    std::vector<std::string> wordsToAdd = {s4, s5};
+    wc.addWords(wordsToAdd);
+
+    ASSERT_TRUE(wc.getList()[0] == e3);
+    ASSERT_TRUE(*wc.getList()[1] == s4);
+    ASSERT_TRUE((int)wc.getList()[1] == 1);
+    ASSERT_TRUE(wc.getList()[2] == e2);
+    ASSERT_TRUE(wc.getList()[3] == e1);
+    ASSERT_TRUE(*wc.getList()[4] == s5);
+    ASSERT_TRUE((int)wc.getList()[4] == 1);
+}
+
+TEST(word_counter, addWords_from_vector_of_entries)
+{
+    std::string s1 = "ccc";
+    entry e1(s1, 1);
+    std::string s2 = "bbb";
+    entry e2(s2, 20);
+    std::string s3 = "aaa";
+    entry e3(s3, 3);
+
+    std::vector<entry> arr = {e1, e2, e3};
+    word_counter wc(arr);
+
+    std::string s4 = "aba";
+    entry e4(s4, 5);
+    std::string s5 = "eee";
+    entry e5(s5, 7);
+    std::vector<entry> wordsToAdd = {e4, e5};
+    wc.addWords(wordsToAdd);
+
+    ASSERT_TRUE(wc.getList()[0] == e3);
+    ASSERT_TRUE(wc.getList()[1] == e4);
+    ASSERT_TRUE(wc.getList()[2] == e2);
+    ASSERT_TRUE(wc.getList()[3] == e1);
+    ASSERT_TRUE(wc.getList()[4] == e5);
+}
+
+TEST(word_counter, addWords_from_stream)
+{
+    std::string s1 = "ccc";
+    entry e1(s1, 1);
+    std::string s2 = "bbb";
+    entry e2(s2, 20);
+    std::string s3 = "aaa";
+    entry e3(s3, 3);
+
+    std::vector<entry> arr = {e1, e2, e3};
+    word_counter wc(arr);
+
+    std::stringstream ss;
+
+    ss << "aaa"
+       << "zzz"
+       << "bbb"
+       << "aba";
+    wc.addWords(ss);
+
+    ASSERT_TRUE(*wc.getList()[0] == "aaa");
+    ASSERT_TRUE((int)wc.getList()[0] == 4);
+    ASSERT_TRUE(*wc.getList()[1] == "aba");
+    ASSERT_TRUE((int)wc.getList()[1] == 1);
+    ASSERT_TRUE(*wc.getList()[2] == "bbb");
+    ASSERT_TRUE((int)wc.getList()[2] == 21);
+    ASSERT_TRUE(*wc.getList()[3] == "ccc");
+    ASSERT_TRUE((int)wc.getList()[3] == 1);
+}
+
+TEST(word_counter, clearCounter)
+{
+    std::string s1 = "ccc";
+    entry e1(s1, 1);
+    std::string s2 = "bbb";
+    entry e2(s2, 20);
+    std::string s3 = "aaa";
+    entry e3(s3, 3);
+
+    std::vector<entry> arr = {e1, e2, e3};
+    word_counter wc(arr);
+
+    wc.clearCounter();
+    ASSERT_TRUE(wc.isEmpty());
+}
+
+TEST(word_counter, addAndSaveOperator)
+{
+    std::string s1 = "ccc";
+    entry e1(s1, 1);
+    std::string s2 = "aaa";
+    entry e2(s2, 20);
+
+    std::string s3 = "aaa";
+    entry e3(s3, 3);
+    std::string s4 = "bbb";
+    entry e4(s4, 7);
+
+    std::vector<entry> arr1 = {e1, e2};
+    std::vector<entry> arr2 = {e3, e4};
+    word_counter wc1(arr1);
+    word_counter wc2(arr2);
+
+    wc1 += wc2;
+
+    ASSERT_TRUE(*wc1.getList()[0] == "aaa");
+    ASSERT_TRUE((int)wc1.getList()[0] == 23);
+    ASSERT_TRUE(*wc1.getList()[1] == "bbb");
+    ASSERT_TRUE((int)wc1.getList()[1] == 7);
+    ASSERT_TRUE(*wc1.getList()[2] == "ccc");
+    ASSERT_TRUE((int)wc1.getList()[2] == 1);
 }

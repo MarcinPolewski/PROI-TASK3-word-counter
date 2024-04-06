0ebfca56c5f0014a290a950ec07cf5b781652633
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-04-06 13:49:01 +0200

    implemented main file for demonstrating basic functionality

--- main.cpp
+++ main.cpp
@@ -1,25 +1,86 @@
 #include "word_counter.h"
+
 #include <fstream>
+#include <stdexcept>
+
+std::ifstream openFileForReading(std::string filePath)
+{
+    std::ifstream fileReader(filePath);
+    if (!fileReader.good())
+    {
+        throw std::runtime_error("could not open file");
+    }
+    return fileReader;
+}
+
+std::ofstream openFileForWriting(std::string filePath)
+{
+    std::ofstream fileReader(filePath);
+    if (!fileReader.good())
+    {
+        throw std::runtime_error("could not open file");
+    }
+    return fileReader;
+}
 
 int main()
 {
+    word_counter counter;
+    // std::ifstream fileReader;
+    std::ofstream fileWriter;
+    // 1. load word_counter status from file
+    // try
+    // {
+    //     std::ifstream fileReader = openFileForReading("wordCounterState.txt");
+    //     fileReader >> counter;
+    //     fileReader.close();
+    // }
+    // catch (...)
+    // {
+    //     std::cout << "opening wordCounterState.txt for reading unsuccesssful\n";
+    // }
 
-    word_counter wc = word_counter();
-    std::string s1 = "baba", s2 = "gaga";
-    wc.addWord(s1);
-    wc.addWord(s2);
+    // 2. read moby dick
+    try
+    {
+        std::ifstream fileReader = openFileForReading("moby_dick.txt");
+        counter.addWords(fileReader);
+        fileReader.close();
+    }
+    catch (...)
+    {
+        std::cout << "opening moby_dick.txt for reading unsuccesssful\n";
+    }
 
-    std::cout << wc;
+    // 3. write  word_counter to file(erasing previous contents)
+    try
+    {
+        fileWriter = openFileForWriting("wordCounterState.txt");
+        fileWriter << counter;
+        fileWriter.close();
+    }
+    catch (...)
+    {
+        std::cout << "opening wordCounterState.txt for writing unsuccessful\n";
+    }
 
-    // std::cout << "start\n";
+    // 4. display statistics on cout
+    std::cout << "top 10 least popular words:\n";
+    int cnt = 0;
+    for (auto it = counter.freqBegin(); it != counter.freqEnd() && cnt <= 10; it++, cnt++)
+    {
+        std::cout << *it << '\n';
+    }
 
-    // std::ofstream fileHandle("counterOutput.txt");
+    std::cout << "\nmost popular word:\n";
+    std::cout << counter.highesCount() << '\n';
 
-    // fileHandle << "test";
+    std::cout << "\ntotal sum of words:\n";
+    std::cout << counter.numberOfWords() << '\n';
 
-    // fileHandle.close();
-
-    // std::cout << "end\n";
+    std::cout << "\ndoes stream have words:\n";
+    std::cout << "banana: " << (counter.hasWord("banana") ? "yes" : "no") << '\n';
+    std::cout << "circumambulate: " << (counter.hasWord("circumambulate") ? "yes" : "no") << '\n';
 
     return 0;
 }
--- word_counter.h
+++ word_counter.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <stdexcept>
 
 class word_counter
 {
